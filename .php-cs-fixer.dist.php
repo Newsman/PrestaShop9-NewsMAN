82cb5d414dc2d91dc63c55b6c0e2b9045c6c211f
Daniel Ifrim <daniel.ifrim7@gmail.com>
2026-04-06 18:32:28 +0300

    Disable blank_line_after_opening_tag CS rule to match marketplace requirements

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -4,6 +4,7 @@ $config = new class() extends PrestaShop\CodingStandards\CsFixer\Config {
     public function getRules(): array
     {
         return array_merge(parent::getRules(), [
+            'blank_line_after_opening_tag' => false,
             'nullable_type_declaration_for_default_null_value' => ['use_nullable_type_declaration' => false],
         ]);
     }
